d05bf6a18bb25018fb2816d57b0bdebeac163115
Mahesh Sundaram <mahesh@omkara.dev>
2021-02-27 16:07:48 -1000

    ignore prettier format

--- src/red-hibiscus.ts
+++ src/red-hibiscus.ts
@@ -1,17 +1,17 @@
-export type car<list extends unknown[]> = list extends [
-  infer head,
-  ...infer _tail
-]
-  ? head
-  : list;
+// prettier-ignore
+export type car<list extends unknown[]> =
+  list extends [infer head, ...infer _tail]
+    ? head
+    : list;
 
-export type cdr<list extends unknown[]> = list extends [
-  infer _head,
-  ...infer tail
-]
-  ? tail
-  : list;
+// prettier-ignore
+export type cdr<list extends unknown[]> =
+  list extends [infer _head, ...infer tail]
+    ? tail
+    : list;
 
-export type cons<item, list extends unknown[] = []> = list extends []
-  ? [item]
-  : [item, ...list];
+// prettier-ignore
+export type cons<item, list extends unknown[] = []> =
+  list extends []
+    ? [item]
+    : [item, ...list];
